519c2e041342883b9a7d5e9f4306da66278dfd71
Krisna Gusti <k_gusti@hotmail.com>
2022-07-03 18:21:54 +0930

    updated sum_int_array function

--- sum_int_array.c
+++ sum_int_array.c
@@ -9,10 +9,50 @@
  * @param array integer array to sum
  * @return int - sum
  */
-int sum_int_array(int* array)
+int sum_int_array(const int array[], const int SIZE)
 {
-    const int SIZE = sizeof(array) / sizeof(int);
+    int NUM_CHILDREN;
     
+    if ( SIZE > 3 ) {
+        NUM_CHILDREN = SIZE / 4;
+    }
+    else {
+        NUM_CHILDREN = 1;
+    }
+
+    pid_t child;
+    pid_t* children;
+    int status;
+    int sum = 0;
+
+    for ( int i = 0; i < NUM_CHILDREN; ++i ) {
+        child = fork();
+        if ( child == -1 ) {
+            printf("Error creating child process.\n");
+            _exit(-1);
+        }
+        else if ( child == 0 ) {
+
+        }
+        else {
+            children[i] = child;
+        }
+    }
+
+    for ( int i = 0; i < NUM_CHILDREN; ++i ) {
+        child = children[i];
+        
+        if ( waitpid(child, &status, 0) < 0 ) {
+            printf("Error - No child to wait for!");
+            return 0;
+        }
+        else if (WEXITSTATUS(status) >= 0) {
+
+        }
+
+    }
+
+    return sum;
 }
 
 // Driver for testing
@@ -20,9 +60,7 @@ int main(int argc, char const *argv[])
 {
     int numbers[] = {1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4};
 
-    int sum = sum_int_array(&numbers);
-
-    printf("Sum: %d", sum);
+    printf("Sum: %d\n", sum_int_array(numbers, (sizeof(numbers)/sizeof(int))));
 
     return 0;
 }
